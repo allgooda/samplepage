1e514e5b639a0a4c3bd5e2d2dd6bcca1d0917bff
Adam Allgood <allgoodadam@gmail.com>
2016-03-15 16:07:49 -0700

    added username check on serverside

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var env = require("../config/environment");
 
 
+
 var index = function(req, res, next) {
   User.find({}, function(error, users) {
     res.json(users);
@@ -11,13 +12,49 @@ var index = function(req, res, next) {
 
 var create = function(req, res, next) {
   console.log("creating user");
-  User.create(req.body, function(err, user){
-    console.log(user);
-    if(err) {
-      res.send(err);
-    }
-      res.json(user);
+
+  var allUsers = {};
+
+  User.find({},function (err, user) {
+    allUsers[user._id] = user;
+    console.log(req.body.username);
+    checkUsername(allUsers);
   });
+
+  var checkUsername = function(users) {
+
+    var check = 0;
+    for(var i = 0; i<users.undefined.length; i++){
+      if(users.undefined[i].username == req.body.username) {
+        check = check + 1;
+        console.log('match');
+
+      }
+    }
+    console.log(check);
+    if(check === 0){ //username is not taken
+      console.log('yup');
+      createUser();
+    } else {
+      res.send('user taken');
+    }
+
+
+
+  }
+
+  var createUser = function () {
+    console.log('creating');
+    User.create(req.body, function(err, user){
+      console.log(user);
+      if(err) {
+        res.send(err);
+      }
+        res.json(user);
+    });
+
+  }
+
 }
 
 
--- public/js/samplepage.module.js
+++ public/js/samplepage.module.js
@@ -11,11 +11,18 @@
       vm.newUser = {};
 
       vm.submitUser = submitUser;
+      vm.lineOne = 'Choose a Username';
+      vm.lineTwo = 'Must be at least 8 characters.';
 
       function submitUser() {
         console.log('clicked');
         $http
-          .post("/users", vm.newUser);
+          .post("/users", vm.newUser)
+          .then(function (response) {
+            if(!response.data.username) {
+              vm.lineOne = "Username already taken";
+            }
+          });
       }
     }
 
