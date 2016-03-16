4034ad03aac65560dd76551efcb041ae5c637add
Adam Allgood <allgoodadam@gmail.com>
2016-03-15 20:56:25 -0700

    building and styling the footer

--- public/js/samplepage.module.js
+++ public/js/samplepage.module.js
@@ -14,6 +14,22 @@
       vm.lineOne = 'Choose a Username';
       vm.lineTwo = 'Must be at least 8 characters.';
 
+      vm.linewidthone = '135px';
+
+      vm.linewidthtwo = '184px';
+
+      function checkLengths() {
+
+      if(vm.lineOne.length === 22) {
+        vm.linewidthone = '162px';
+        vm.linewidthtwo = '145px'
+      } else if (vm.lineOne.length === 17) {
+        vm.linewidthone = "126px"
+        vm.linewidthtwo = "60px"
+      }
+
+      }
+
       function submitUser() {
         console.log('clicked');
         $http
@@ -21,6 +37,12 @@
           .then(function (response) {
             if(!response.data.username) {
               vm.lineOne = "Username already taken";
+              vm.lineTwo = "Please try another one.";
+              checkLengths();
+            } else {
+              vm.lineOne = "Username created.";
+              vm.lineTwo = "Success!"
+              checkLengths();
             }
           });
       }
